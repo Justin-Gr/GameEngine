91b19012d0f48ab1017c823dd4c1d29139ac4467
Justin Grosjean <justin.grosjean@etu.univ-lyon1.fr>
2020-07-07 17:34:18 +0200

    Première implémentation shaders

--- src/engineTester/MainGameLoop.java
+++ src/engineTester/MainGameLoop.java
@@ -6,39 +6,41 @@ import renderEngine.DisplayManager;
 import renderEngine.Loader;
 import renderEngine.RawModel;
 import renderEngine.Renderer;
+import shaders.StaticShader;
 
 public class MainGameLoop {
 
 	public static void main(String[] args) {
 		
 		DisplayManager.createDisplay();
-		
 		Loader loader = new Loader();
 		Renderer renderer = new Renderer();
+		StaticShader shader = new StaticShader();
 		
-		// OpenGL expects vertices to be defined counter clockwise by default
 		float[] vertices = {
-			// Bottom left triangle
-			-0.5f,  0.5f, 0f,
-			-0.5f, -0.5f, 0f,
-			 0.5f, -0.5f, 0f,
-			
-			// Top right triangle
-			 0.5f, -0.5f, 0f,
-			 0.5f,  0.5f, 0f,
-			-0.5f,  0.5f, 0f
+			-0.5f,  0.5f, 0,
+			-0.5f, -0.5f, 0,
+			 0.5f, -0.5f, 0,
+			 0.5f,  0.5f, 0
 		};
 		
-		RawModel model = loader.loadToVAO(vertices);
+		int[] indices = {
+			0, 1, 3,
+			3, 1, 2
+		};
+		
+		RawModel model = loader.loadToVAO(vertices, indices);
 		
 		while(!Display.isCloseRequested()) {
 			renderer.prepare();
 			//game logic
+			shader.start();
 			renderer.render(model);
+			shader.stop();
 			DisplayManager.updateDisplay();
-			
 		}
 		
+		shader.cleanUp();
 		loader.cleanUp();
 		DisplayManager.closeDisplay();
 
