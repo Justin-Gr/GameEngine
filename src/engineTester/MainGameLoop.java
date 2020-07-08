31e3e5ca1ce728e4316c9a2fae393cd61e61ea2d
Justin Grosjean <justin.grosjean@etu.univ-lyon1.fr>
2020-07-08 17:55:13 +0200

    Matrice de transfo, projection et vue + caméra

--- src/engineTester/MainGameLoop.java
+++ src/engineTester/MainGameLoop.java
@@ -1,7 +1,10 @@
 package engineTester;
 
 import org.lwjgl.opengl.Display;
+import org.lwjgl.util.vector.Vector3f;
 
+import entities.Camera;
+import entities.Entity;
 import models.RawModel;
 import models.TexturedModel;
 import renderEngine.DisplayManager;
@@ -15,38 +18,106 @@ public class MainGameLoop {
 	public static void main(String[] args) {
 		
 		DisplayManager.createDisplay();
+		
 		Loader loader = new Loader();
-		Renderer renderer = new Renderer();
 		StaticShader shader = new StaticShader();
+		Renderer renderer = new Renderer(shader);
 		
-		float[] vertices = {
-			-0.5f,  0.5f, 0,
-			-0.5f, -0.5f, 0,
-			 0.5f, -0.5f, 0,
-			 0.5f,  0.5f, 0
-		};
-		
-		int[] indices = {
-			0, 1, 3,
-			3, 1, 2
+		float[] vertices = {			
+				-0.5f,0.5f,-0.5f,	
+				-0.5f,-0.5f,-0.5f,	
+				0.5f,-0.5f,-0.5f,	
+				0.5f,0.5f,-0.5f,		
+				
+				-0.5f,0.5f,0.5f,	
+				-0.5f,-0.5f,0.5f,	
+				0.5f,-0.5f,0.5f,	
+				0.5f,0.5f,0.5f,
+				
+				0.5f,0.5f,-0.5f,	
+				0.5f,-0.5f,-0.5f,	
+				0.5f,-0.5f,0.5f,	
+				0.5f,0.5f,0.5f,
+				
+				-0.5f,0.5f,-0.5f,	
+				-0.5f,-0.5f,-0.5f,	
+				-0.5f,-0.5f,0.5f,	
+				-0.5f,0.5f,0.5f,
+				
+				-0.5f,0.5f,0.5f,
+				-0.5f,0.5f,-0.5f,
+				0.5f,0.5f,-0.5f,
+				0.5f,0.5f,0.5f,
+				
+				-0.5f,-0.5f,0.5f,
+				-0.5f,-0.5f,-0.5f,
+				0.5f,-0.5f,-0.5f,
+				0.5f,-0.5f,0.5f
 		};
 		
 		float[] textureCoords = {
-				0, 0,
-				0, 1,
-				1, 1,
-				1, 0
+				0,0,
+				0,1,
+				1,1,
+				1,0,
+				
+				0,0,
+				0,1,
+				1,1,
+				1,0,
+				
+				0,0,
+				0,1,
+				1,1,
+				1,0,
+				
+				0,0,
+				0,1,
+				1,1,
+				1,0,
+				
+				0,0,
+				0,1,
+				1,1,
+				1,0,
+				
+				0,0,
+				0,1,
+				1,1,
+				1,0	
+		};
+		
+		int[] indices = {
+				0,1,3,	
+				3,1,2,	
+				4,5,7,
+				7,5,6,
+				8,9,11,
+				11,9,10,
+				12,13,15,
+				15,13,14,	
+				16,17,19,
+				19,17,18,
+				20,21,23,
+				23,21,22
 		};
 		
 		RawModel model = loader.loadToVAO(vertices, textureCoords, indices);
 		ModelTexture texture = new ModelTexture(loader.loadTexture("lisa"));
 		TexturedModel texturedModel = new TexturedModel(model, texture);
 		
+		Entity entity = new Entity(texturedModel, new Vector3f(0, 0, -5), 0, 0, 0, 1);
+		
+		Camera camera = new Camera();
+		
 		while(!Display.isCloseRequested()) {
+			entity.increasePosition(0f, 0f, 0f);
+			entity.increaseRotation(1f, 1f, 0f);
+			camera.move();
 			renderer.prepare();
-			//game logic
 			shader.start();
-			renderer.render(texturedModel);
+			shader.loadViewMatrix(camera);
+			renderer.render(entity, shader);
 			shader.stop();
 			DisplayManager.updateDisplay();
 		}
--- src/renderEngine/DisplayManager.java
+++ src/renderEngine/DisplayManager.java
@@ -14,10 +14,9 @@ public class DisplayManager {
 	private static final int FPS_CAP = 120;
 	
 	public static void createDisplay() {
-		
-		ContextAttribs attribs =  new ContextAttribs(3, 2);
-		attribs.withForwardCompatible(true);
-		attribs.withProfileCore(true);
+		ContextAttribs attribs =  new ContextAttribs(3, 2)
+		.withForwardCompatible(true)
+		.withProfileCore(true);
 		
 		try {
 			Display.setDisplayMode(new DisplayMode(WIDTH, HEIGHT));
@@ -28,20 +27,15 @@ public class DisplayManager {
 		}
 		
 		GL11.glViewport(0, 0, WIDTH, HEIGHT);
-		
 	}
 	
 	public static void updateDisplay() {
-		
 		Display.sync(FPS_CAP);
 		Display.update();
-		
 	}
 	
 	public static void closeDisplay() {
-		
-		Display.destroy();
-		
+		Display.destroy();	
 	}
 	
 }
